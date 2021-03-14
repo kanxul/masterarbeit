2c82fe51cbf21af2046381f04b30a42f2374b25a
kanxul <kanxul@googlemail.com>
2021-03-14 16:34:49 +0100

    Adding some learnings of OOP

--- oops_basics_03.py
+++ oops_basics_03.py
@@ -2,6 +2,8 @@
 
 class Employee:
 
+    __gender = "female"
+
     # Class Constructor
     def __init__(self, first_name, last_name):
         self.first_name = first_name
@@ -11,10 +13,10 @@ class Employee:
 
     # Methods
     def get_monthly_gross(self):
-        return self.base_annual_salary / 12
+        return self.__base_annual_salary / 12
     
     def get_standard_annual_bonus_amount(self):
-        return self.base_annual_salary * (self.__bonus_percentage / 100)
+        return self.__base_annual_salary * (self.__bonus_percentage / 100)
 
     # Getters
     @property
@@ -28,14 +30,14 @@ class Employee:
     # Setters
     @base_annual_salary.setter
     def base_annual_salery(self, base_annual_salary):
-        if 45000.00 >= base_annual_salary <= 120000.00:
+        if 45000.00 <= base_annual_salary <= 120000.00:
             self.__base_annual_salary = base_annual_salary
         else:
             print("Annual base salary must be between 45k abd 120k!")
 
     @bonus_percentage.setter
     def bonus_percentage(self, bonus_percentage):
-        if 5.0 >= bonus_percentage <= 21:
+        if 5.0 <= bonus_percentage <= 21:
             self.__bonus_percentage = bonus_percentage
         else:
             print("Bonus percentages must be between 5% an 21 %") 
@@ -46,6 +48,7 @@ class Employee:
     def get_info_employee(self):
         print("Employee Info".center(58, "-"))
         print("Name:                 {1}, {0} ".format(self.first_name, self.last_name))
+        print("Gender:               {}".format(self.__gender))
         print("Emplyee ID:           {}".format(self.__get_employee_id()))
         print("Base Ann. Salary:     $ {:8.2f}".format(self.base_annual_salary))
         print("Bonus percentage:       {:8.2f} %".format(self.bonus_percentage))
@@ -55,9 +58,16 @@ class Employee:
 def main():
     print("Hello from main()!")
     empl1 = Employee("Kara", "Smith")
-    empl1.base_annual_salery = 20000.00
-    empl1.bonus_percentage = 22
+    empl1.base_annual_salery = 55000.00
+    empl1.bonus_percentage = 5
+    empl1.__gender = "male"             # this will be not assigned!
     empl1.get_info_employee()
+
+    empl2 = Employee("Susi", "Schmusi")
+    empl2.base_annual_salery = 55000.00
+    empl2.bonus_percentage = 5
+    empl2.__gender = "male"
+    empl2.get_info_employee()
     
 
 
--- oops_basics_03_client.py
+++ oops_basics_03_client.py
@@ -2,6 +2,8 @@
 
 from oops_basics_03 import Employee
 
-employee1 = Employee("Hans-Peter", "Klossek", 15000)
+employee1 = Employee("Hans-Peter", "Klossek")
 
+employee1.base_annual_salery = 100000
+employee1.bonus_percentage = 10
 employee1.get_info_employee()
